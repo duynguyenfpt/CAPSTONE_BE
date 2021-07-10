2ace3cb34569791c2ecbbd182062e04c62d97929
Long Vu <vulong22121999@gmail.com>
2021-07-10 20:07:33 +0700

    crontab

--- src/main/java/com/web_service/Application.java
+++ src/main/java/com/web_service/Application.java
@@ -6,9 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
-
+@EnableScheduling
 public class Application extends SpringBootServletInitializer {
 	public static void main (String[] args) {
 		SpringApplication.run(Application.class, args);
---
+++ src/main/java/com/web_service/crontab/service/HeadRequestService.java
@@ -0,0 +1,18 @@
+//package com.web_service.crontab.service;
+//
+//import java.net.http.HttpHeaders;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.scheduling.config.ScheduledTask;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class HeadRequestService {
+//	private static final Logger log = LoggerFactory.getLogger(ScheduledTask.class);
+//	private HttpHeaders headers;
+//	
+//	public String doHeadRequest() {
+//		Httpc
+//	}
+//}
